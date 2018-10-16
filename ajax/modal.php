3aeb6b73208d8b6cf102dea279b52eab0f460ff3
yeffasol <yeffasol@mail.com>
2018-10-16 14:46:32 +0300

    Add rating

--- ajax/modal.php
+++
@@ -1,7 +0,0 @@
-<div>
-	3
-</div>
-<script>
-	console.log("modal open");
-</script>
-
--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-$(document).ready(function () {
-    addVoidForLinks($("a"));
-    setEqualHeight($(".row>.col"));
-    hamburger('js-hamburger', "js-menu");
-    addPhoneMask('.js-phone');// https://unmanner.github.io/imaskjs/guide.html
-    $('.modaal-ajax').modaal({
-        type: 'ajax'
+(function() {
+    var inputs = document.querySelectorAll(".rating__input[name='rating']");
+    var ratingValue=document.querySelector(".js-star-value");
+
+    Array.prototype.forEach.call(inputs, function(input) {
+        input.addEventListener("change", function(){
+            ratingValue.value=input.value;
+        })
     });
-});
+})();
